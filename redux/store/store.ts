7d20ccfc81c254a8a17f72bed465489bcba4fc08
martiros888 <harutunyan.martiros@mail.ru>
2021-04-02 01:53:30 +0400

    changed redux

--- redux/store/store.ts
+++ redux/store/store.ts
@@ -1,21 +1,16 @@
 import {combineReducers} from  'redux';
 import Reducer1 from '../reducer/reducer';
 import thunkMiddleware from 'redux-thunk';
-import { createStore, applyMiddleware, compose } from 'redux';
-declare global {
-    interface Window {
-      __REDUX_DEVTOOLS_EXTENSION_COMPOSE__?: typeof compose;
-    }
-}
-
+import { createStore, applyMiddleware } from 'redux';
+import { composeWithDevTools } from 'redux-devtools-extension';
 const rootReducer = combineReducers({
     Reducer1
 })
 
-// const composeEnhancers = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose;
-// const store = createStore(rootReducer, composeEnhancers(applyMiddleware(thunkMiddleware)));
+const middleware = [thunkMiddleware]
 
-const store = createStore(rootReducer)
+const store = createStore(rootReducer, composeWithDevTools(applyMiddleware(...middleware)))
+
 
 
 export default store
